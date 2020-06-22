8ab5def176472ef9363fdfe7928f9f15bfe684cf
Nguyen Van Loi <Loinguyen.hg.n@gmail.com>
2020-06-22 15:19:38 +0700

    add newtonjsonsoft

--- truckapi/Startup.cs
+++ truckapi/Startup.cs
@@ -25,7 +25,10 @@ namespace truckapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
             services.AddDbContext<MyDbContext>();
         }
 
