22fa31f5b914b608a01ba3d43fa98d8c151c83b0
Nguyen Van Loi <Loinguyen.hg.n@gmail.com>
2020-07-19 23:15:32 +0700

    add db

---
+++ truckapi/Migrations/20200719160328_2stCreate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace truckapi.Migrations
+{
+    public partial class _2stCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "District",
+                table: "Address",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Province",
+                table: "Address",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Ward",
+                table: "Address",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "District",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "Province",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "Ward",
+                table: "Address");
+        }
+    }
+}
