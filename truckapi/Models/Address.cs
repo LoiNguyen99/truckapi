ba1ec8fa2f1947a304b241dfe1bd552efae32b41
Nguyen Van Loi <Loinguyen.hg.n@gmail.com>
2020-07-19 23:02:00 +0700

    add Address detail

--- truckapi/Models/Address.cs
+++ truckapi/Models/Address.cs
@@ -9,7 +9,9 @@ namespace truckapi.Models
     {
         public int AddressId { get; set; }
         public String StreetName { get; set; }
-
+        public String Province { get; set; }
+        public String District { get; set; }
+        public String Ward { get; set; }
         public ICollection<Place> Places { get; set; }
     }
 }
